65b34cef694c6bec613e15a417a746e361aac05e
Mateusz <97304124+itamm15@users.noreply.github.com>
2023-04-08 09:42:52 +0200

    run_all_sfml()

--- custom_bash_commands.sh
+++ custom_bash_commands.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# SFML code runner
+# C++
 function run_sfml(){
   g++ $1 -I/opt/homebrew/Cellar/sfml/2.5.1_2/include/ -L/opt/homebrew/Cellar/sfml/2.5.1_2/lib/ -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 }
 
+function run_all_sfml() {
+  g++ *.cpp -I/opt/homebrew/Cellar/sfml/2.5.1_2/include/ -L/opt/homebrew/Cellar/sfml/2.5.1_2/lib/ -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+}
+
+
+# Elixir
 function mix_all(){
   echo "mix format"
   mix format
@@ -16,6 +22,13 @@ function mix_all(){
   mix test
 }
 
+# Git
+function count_lines_from_git(){
+# If you want to use that function, just change the author with yours nick
+git log --author="itamm15" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }' -
+}
+
+# Misc
 function create_user(){
   echo "User creation function"
   sudo dscl . -create /Users/$1
@@ -33,7 +46,3 @@ function create_user(){
   fi
 }
 
-function count_lines_from_git(){
-# If you want to use that function, just change the author with yours nick
-git log --author="itamm15" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }' -
-}
